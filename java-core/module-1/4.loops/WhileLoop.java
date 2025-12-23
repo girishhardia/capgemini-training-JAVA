7151b9b3b0a5a56e2816aefe43c9bdf7a110551a
girishhardia <girishhardia@gmail.com>
2025-12-23 09:52:49 +0530

    Loops

---
+++ java-core/module-1/4.loops/WhileLoop.java
@@ -0,0 +1,32 @@
+class WhileLoop{
+	public static void main(String[] args) {
+		int i = -123;
+		while(i>=-129){
+		System.out.print(i);
+		i--;
+		System.out.print(" ");
+	}
+		System.out.println("");
+		i = 79;
+		while(i<=84){
+		System.out.print(i);
+		i++;
+		System.out.print(" ");
+	}
+		System.out.println("");
+		i = -110;
+		while(i<=-100){
+		System.out.print(i);
+		i++;
+		System.out.print(" ");
+	}
+		System.out.println("");
+		i = 0;
+		while(i>=-9){
+		System.out.print(i);
+		i--;
+		System.out.print(" ");
+	}
+
+	}
+}
