1217c2d5f7fa01309ee3bf8922c9ee3d2a7f49c1
girishhardia <girishhardia@gmail.com>
2026-01-07 14:45:56 +0530

    Polymorphism

--- java-core/module-2/6.polymorphism/Intro.java
+++ java-core/module-2/6.polymorphism/Intro.java
@@ -8,7 +8,10 @@
 //      We can achieve compile time polymorphism using
 //      1. Method overloading
 //      2. Constructor overloading
-//      3. Variable shadowing
+//      3. Variable shadowing : 
+//          When a variable in a subclass has the same name as a variable in its superclass.
+//          The variable in the subclass will shadow the variable in the superclass.
+//          Which variavle to be called is decided during compile time.
 //      4. Method shadowing
 //      
 // 2. Run time polymorphism
@@ -18,7 +21,42 @@
 //      1. Method overriding
 //
 
+// why we use variable shadowing
+// to access the variable of superclass
+// where we use variable shadowing
+// we use variable shadowing while upcasting, downcasting and method overriding
+
+class Father {
+    int a = 10;
+    static int b = 100;
+}
+
+class Son extends Father {
+    int a = 20;
+    static int b = 200;
+}
+
 
 public class Intro {
-    
+    public static void main(String[] args) {
+        System.out.println("Main start");
+        System.out.println("--Father object stored in Father ref variable--");
+        Father ref1 = new Father();
+        System.out.println(ref1.a);
+        System.out.println(ref1.b);
+        System.out.println("--Son object stored in Son ref variable--");
+        Son ref2 = new Son();
+        System.out.println(ref2.a);
+        System.out.println(ref2.b);
+        System.out.println("---Upcasting---");
+        Father ref3 = new Son();
+        System.out.println(ref3.a);
+        System.out.println(ref.b);
+        System.out.println("--Downcasting---");
+        Son ref4 = (Son) ref3;
+        System.out.println(ref4.a);
+        System.out.println(ref4.b);
+
+        System.out.println("Main end");
+    }
 }
