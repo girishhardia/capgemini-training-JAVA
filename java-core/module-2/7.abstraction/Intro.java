bf2cdcd78925e2a4d8c5aa7cee7b9d44cddfa0f3
girishhardia <girishhardia@gmail.com>
2026-01-08 10:49:51 +0530

    abstraction

---
+++ java-core/module-2/7.abstraction/Intro.java
@@ -0,0 +1,81 @@
+// Abstraction: Hiding the implementation details and showing only the functionality is known as abstraction.
+// It is a process of hiding the implementation details and showing only the functionality.
+
+// Types of Abstraction
+// 1. Abstract class    : A class which is declared with abstract keyword is known as abstract class.
+//                         It can have abstract methods (without body) and non-abstract methods (with body).
+//                          
+// 2. Interface         : An interface is a reference type, similar to a class, that can contain only constants, method signatures, default methods, static methods, and nested types.
+//                         It can have only abstract methods (without body) and static methods (with body).
+
+// why we use abstract class
+// to hide the implementation details
+// where we use abstract class
+// we use abstract class while inheritance (upcasting, downcasting), method overriding and method shadowing
+
+
+/*
+Abstraction: 
+    Abtraction means showing only essential features and hiding implementation details.
+    in simple words, displaying the things which are required and hiding the things which are not required.
+
+How Abstraction is achieved in Java?
+    1. Using Abstract class
+    2. Using Interface
+
+Concrete method :
+    A method which has method body and it contain implementation logic.
+
+concrete class :
+    A concrete class is a normal class which is not declared with abstract keyword, complete with methods implementation, and it allows to create objects.
+    It can have concrete methods (with body) and abstract methods (without body).
+
+Abstract method :
+    A method which does not have method body and it does not contain implementation logic.
+
+Abstract class :
+    A class which is declared with abstract keyword is known as abstract class.
+    It can have abstract methods (without body) and non-abstract methods (with body).
+
+
+
+Interface :
+    An interface is a reference type, similar to a class, that can contain only constants, method signatures, default methods, static methods, and nested types.
+    It can have only abstract methods (without body) and static methods (with body).
+
+
+
+*/
+
+abstract class Father {
+    abstract public void test();
+    abstract public void demo(int a, int b);
+}
+
+class Son extends Father {
+    @Override
+    public void test(){
+        System.out.println("Son class test method");
+    }
+    @Override
+    public void demo(int a, int b){
+        System.out.println("Son class demo method");
+    }
+    public void show(){
+        System.out.println("Son class show method");
+    }
+}
+
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Main start");
+        //Father ref = new Father(); CTE cannot instantiate the type Father
+        Father ref = new Son();
+        ref.test();
+        System.out.println(ref.demo(0, 0));
+        //Downcasting
+        Son ref2 = (Son) ref;
+        ref2.show();
+        System.out.println("Main end");
+    }
+}
